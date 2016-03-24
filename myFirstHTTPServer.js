81b6c1eedbd11320db50cbae97758a927c1482b9
Hon Lam <lamh85@gmail.com>
2016-03-23 20:52:20 -0700

    Debugged the routing: Route-response functions were in the wrong block.

--- myFirstHTTPServer.js
+++ myFirstHTTPServer.js
@@ -15,22 +15,24 @@ function handleRequest(request, response){
     } catch(err) {
         console.log(err);
     }
+
+    //For all your static (js/css/images/etc.) set the directory name (relative path).
+    dispatcher.setStatic('resources');
+
+    //A sample GET request    
+    dispatcher.onGet("/page1", function(req, res) {
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.end('Page One');
+    });    
+
+    //A sample POST request
+    dispatcher.onPost("/post1", function(req, res) {
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.end('Got Post Data');
+    });
+    
 }
 
-//For all your static (js/css/images/etc.) set the directory name (relative path).
-dispatcher.setStatic('resources');
-
-//A sample GET request    
-dispatcher.onGet("/page1", function(req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Page One');
-});    
-
-//A sample POST request
-dispatcher.onPost("/post1", function(req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Got Post Data');
-});
 
 //Create a server
 var server = http.createServer(handleRequest);

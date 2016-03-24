d0873b8a02a89c05696a1171c933477147cfe167
Hon Lam <lamh85@gmail.com>
2016-03-23 20:50:21 -0700

    Added some basic routes.

--- myFirstHTTPServer.js
+++ myFirstHTTPServer.js
@@ -7,8 +7,30 @@ const PORT = 8080;
 
 //We need a function which handles requests and send response
 function handleRequest(request, response){
-    response.end('It Works!! Path Hit: ' + request.url);
+    try {
+        //log the request on console
+        console.log(request.url);
+        //Disptach
+        dispatcher.dispatch(request, response);
+    } catch(err) {
+        console.log(err);
+    }
 }
+
+//For all your static (js/css/images/etc.) set the directory name (relative path).
+dispatcher.setStatic('resources');
+
+//A sample GET request    
+dispatcher.onGet("/page1", function(req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Page One');
+});    
+
+//A sample POST request
+dispatcher.onPost("/post1", function(req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Got Post Data');
+});
 
 //Create a server
 var server = http.createServer(handleRequest);
